8544adab72602faffa878e65a05187b5dbdb310c
Dennis Groß <gdennis91@googlemail.com>
2016-06-13 15:54:51 +0200

    slicing modules created

---
+++ CeramicsSlicer/CeramicsSlicer/SpiralSlicerComponent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace CeramicsSlicer
+{
+    public class SpiralSlicerComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the SpiralSlicerComponent class.
+        /// </summary>
+        public SpiralSlicerComponent()
+          : base("SpiralSlicerComponent", "SpiralSlicer",
+              "Slicing the raw nub file in a point model for spiral path filtering",
+              "Ceramixx", "Slicing")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{b8c3c4e8-b699-4f32-a427-9b0d9caffd79}"); }
+        }
+    }
+}
